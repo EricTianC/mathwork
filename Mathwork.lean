c64f1612e751355a6dc9a0243c14cbe74ecb750b
Eric <erictianc@outlook.com>
2023-06-08 21:53:27 +0800

    优化

--- Mathwork.lean
+++ Mathwork.lean
@@ -37,41 +37,38 @@ theorem tan_add (A B: ℝ) (ha: cos A ≠ 0) (hb: cos B ≠ 0): tan (A + B) = (t
 
 theorem tri_tan_sum_eq_mul (A B C : ℝ) (h: A + B + C = π) (ha: cos A ≠ 0) (hb: cos B ≠ 0) (hc: cos C ≠ 0):
     tan A + tan B + tan C = tan A * tan B * tan C := by
-    have s1 :=
-        calc
-            C = A + B + C - A - B := by ring
-            _ = π - A - B := by rw [h]
-            _ = π - (A + B) := by ring
-
-    have s2 := 
-        calc
-            tan C = - tan (A + B) := by rw [s1, tan_pi_sub]
-            _     = - ((tan A + tan B)/((1: ℝ) - tan A * tan B)) := by rw [tan_add _ _ ha hb]
-    
-    have s3 : tan C * ((1: ℝ) - tan A * tan B) = - (tan A + tan B) := by
-        rw [s2, neg_mul ((tan A + tan B) / (1 - tan A * tan B)) (1 - tan A * tan B)]
-        rw [mul_comm_div, div_self]
-        simp
-        apply_fun λ x: ℝ => x * cos A * cos B
-        ring_nf
-        repeat rw [tan_eq_sin_div_cos]
-        ring_nf
-        rw [calc
-            sin A * cos A * (cos A)⁻¹  * sin B * cos B * (cos B)⁻¹ = sin A * (cos A * (cos A)⁻¹)  * sin B * (cos B * (cos B)⁻¹) := by ring_nf 
-            _ = sin A * 1 * sin B * 1 := by rw [mul_inv_cancel ha, mul_inv_cancel hb],
-            calc
-            -(sin A * 1 * sin B * 1) + cos A * cos B = cos A * cos B - sin A * sin B := by ring_nf,
-            ←cos_add
-            ]
-        apply_fun λ x: ℝ => - cos x at s1
-        rw [cos_pi_sub] at s1
-        simp at s1
-        rw [←s1, neg_ne_zero]
-        assumption
-    ring_nf at s3
-    apply_fun λ x: ℝ => x + tan C * tan A * tan B + tan A + tan B at s3
-    ring_nf at s3
-    rw [calc tan C + tan A + tan B = tan A + tan B + tan C := by ring_nf] at s3
-    rw [s3]
+    have s1 : C = π - (A + B) 
+        := by simp [←h]
+    have s2 : tan C = - ((tan A + tan B)/((1: ℝ) - tan A * tan B)) 
+        := by simp [s1, tan_pi_sub]; refine (tan_add A B ha hb)
+    have s3 : (1: ℝ) - tan A * tan B ≠ 0
+        := by field_simp [tan_eq_sin_div_cos, ←cos_add]; rw [←neg_eq_zero, ←cos_pi_sub (A + B), ←s1]; assumption
+    have s4 : tan C * ((1: ℝ) - tan A * tan B) = - (tan A + tan B) := by
+        field_simp [s2]
+        -- rw [s2, neg_mul ((tan A + tan B) / (1 - tan A * tan B)) (1 - tan A * tan B)]
+        -- rw [mul_comm_div, div_self]
+        -- simp
+        -- apply_fun λ x: ℝ => x * cos A * cos B
+        -- ring_nf
+        -- repeat rw [tan_eq_sin_div_cos]
+        -- ring_nf
+        -- rw [calc
+        --     sin A * cos A * (cos A)⁻¹  * sin B * cos B * (cos B)⁻¹ = sin A * (cos A * (cos A)⁻¹)  * sin B * (cos B * (cos B)⁻¹) := by ring_nf 
+        --     _ = sin A * 1 * sin B * 1 := by rw [mul_inv_cancel ha, mul_inv_cancel hb],
+        --     calc
+        --     -(sin A * 1 * sin B * 1) + cos A * cos B = cos A * cos B - sin A * sin B := by ring_nf,
+        --     ←cos_add
+        --     ]
+        -- apply_fun λ x: ℝ => - cos x at s1
+        -- rw [cos_pi_sub] at s1
+        -- simp at s1
+        -- rw [←s1, neg_ne_zero]
+        -- assumption
+    -- simp at s4
+    ring_nf at s4
+    apply_fun λ x: ℝ => x + tan C * tan A * tan B + tan A + tan B at s4
+    ring_nf at s4
+    rw [calc tan C + tan A + tan B = tan A + tan B + tan C := by ring_nf] at s4
+    rw [s4]
     ring_nf
     exact trivial
