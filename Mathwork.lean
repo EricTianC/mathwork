39155877d173083c08be19bf09c580e49843e582
Eric <erictianc@outlook.com>
2023-06-09 08:20:06 +0800

    clean

--- Mathwork.lean
+++ Mathwork.lean
@@ -24,12 +24,6 @@ theorem sin_sq_minus_sq (A B : ℝ) : sin (A + B) * sin (A - B) = sin (A)*sin (A
 theorem tan_add (A B: ℝ) (ha: cos A ≠ 0) (hb: cos B ≠ 0): tan (A + B) = (tan A + tan B)/((1: ℝ) - tan A * tan B) := by
     simp [tan_eq_sin_div_cos]
     rw [sin_add, cos_add]
-    -- rw [calc
-    --         (sin A / cos A + sin B / cos B) / (1 - sin A / cos A * (sin B / cos B)) = (sin A / cos A + sin B / cos B) / (1 - sin A / cos A * (sin B / cos B)) * 1 := by ring_nf
-    --         _ = (sin A / cos A + sin B / cos B) / (1 - sin A / cos A * (sin B / cos B)) * ((cos A * cos B) / (cos A * cos B)) := by rw [←div_self (mul_ne_zero ha hb)]
-    --         _ = (sin A / cos A + sin B / cos B) * (cos A * cos B) / ((1 - sin A / cos A * (sin B / cos B)) * (cos A * cos B)) := by rw [div_mul_div_comm]
-    --         _ = (((sin A / cos A) * cos A) * cos B + ((sin B / cos B) *  cos B) * cos A) / ((1 - sin A / cos A  * cos A * ((sin B / cos B) * cos B))) := by sorry
-    --     ]
     field_simp
     ring
     
@@ -45,26 +39,6 @@ theorem tri_tan_sum_eq_mul (A B C : ℝ) (h: A + B + C = π) (ha: cos A ≠ 0) (
         := by field_simp [tan_eq_sin_div_cos, ←cos_add]; rw [←neg_eq_zero, ←cos_pi_sub (A + B), ←s1]; assumption
     have s4 : tan C * ((1: ℝ) - tan A * tan B) = - (tan A + tan B) := by
         field_simp [s2]
-        -- rw [s2, neg_mul ((tan A + tan B) / (1 - tan A * tan B)) (1 - tan A * tan B)]
-        -- rw [mul_comm_div, div_self]
-        -- simp
-        -- apply_fun λ x: ℝ => x * cos A * cos B
-        -- ring_nf
-        -- repeat rw [tan_eq_sin_div_cos]
-        -- ring_nf
-        -- rw [calc
-        --     sin A * cos A * (cos A)⁻¹  * sin B * cos B * (cos B)⁻¹ = sin A * (cos A * (cos A)⁻¹)  * sin B * (cos B * (cos B)⁻¹) := by ring_nf 
-        --     _ = sin A * 1 * sin B * 1 := by rw [mul_inv_cancel ha, mul_inv_cancel hb],
-        --     calc
-        --     -(sin A * 1 * sin B * 1) + cos A * cos B = cos A * cos B - sin A * sin B := by ring_nf,
-        --     ←cos_add
-        --     ]
-        -- apply_fun λ x: ℝ => - cos x at s1
-        -- rw [cos_pi_sub] at s1
-        -- simp at s1
-        -- rw [←s1, neg_ne_zero]
-        -- assumption
-    -- simp at s4
     ring_nf at s4
     apply_fun λ x: ℝ => x + tan C * tan A * tan B + tan A + tan B at s4
     ring_nf at s4
